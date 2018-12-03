5e892c0f77f13f0e9ef0ba4c4f6dbd77835346d9
Emre Koc <hi@emroni.com>
2018-12-03 17:29:24 +0700

    Add Symfony4 database:pull task

--- recipe/symfony4.php
+++ recipe/symfony4.php
@@ -12,22 +12,29 @@ task('database:backup', function () {
 });
 
 task('database:download', function () {
-    $file = getLastBackup();
-    download(CURRENT_PATH . $file, PATH . $file);
+    downloadBackup();
 });
 
 task('database:restore', function () {
     $file = getLastBackup();
-    $file = CURRENT_PATH . $file;
     $db = getDatabase();
-
-    run("mysql -Nse 'show tables' -D {$db->name} {$db->credentials} | while read table; do echo \"SET FOREIGN_KEY_CHECKS = 0;DROP TABLE \`\$table\`;SET FOREIGN_KEY_CHECKS = 1;\"; done | mysql {$db->name} {$db->credentials}");
-    run("mysql {$db->credentials} {$db->name} < {$file}");
+    replaceDatabase($db, CURRENT_PATH . $file);
 });
 
-function getDatabase()
+task('database:pull', function () {
+    $file = downloadBackup();
+    $db = getDatabase(true);
+    replaceDatabase($db, PATH . $file, true);
+});
+
+function getDatabase($local = false)
 {
-    $env = run('cd {{deploy_path}}/current && cat .env');
+    if ($local) {
+        $env = runLocally('cat .env');
+    } else {
+        $env = run('cd {{deploy_path}}/current && cat .env');
+    }
+
     preg_match('/^DATABASE_URL=(.*?)$/m', $env, $url);
     $url = $url[1];
 
@@ -36,6 +43,20 @@ function getDatabase()
     $credentials = "--host=\"{$host}\" --user=\"{$user}\" --password=\"{$password}\"";
 
     return (object)compact('user', 'password', 'host', 'port', 'name', 'credentials');
+}
+
+function replaceDatabase($db, $file, $local = false)
+{
+    $drop = "mysql -Nse 'show tables' -D {$db->name} {$db->credentials} | while read table; do echo \"SET FOREIGN_KEY_CHECKS = 0;DROP TABLE \`\$table\`;SET FOREIGN_KEY_CHECKS = 1;\"; done | mysql {$db->name} {$db->credentials}";
+    $import = "mysql {$db->credentials} {$db->name} < {$file}";
+
+    if ($local) {
+        runLocally($drop);
+        runLocally($import);
+    } else {
+        run($drop);
+        run($import);
+    }
 }
 
 function createBackup()
@@ -58,6 +79,16 @@ function getLastBackup()
     if (!$file) {
         createBackup();
     }
+
+    return $file;
+}
+
+function downloadBackup()
+{
+    runLocally('mkdir -p ' . PATH);
+
+    $file = getLastBackup();
+    download(CURRENT_PATH . $file, PATH . $file);
 
     return $file;
 }
