93dbb5bca526027bfd765199e41f79a874bebc46
Emre Koc <hi@emroni.com>
2018-12-03 16:21:41 +0700

    Add Symfony4 database:download task

--- recipe/symfony4.php
+++ recipe/symfony4.php
@@ -4,10 +4,11 @@ namespace Deployer;
 
 require_once 'recipe/common.php';
 
-const PATH = '{{deploy_path}}/current/var/database';
+const PATH = 'var/database/';
+const CURRENT_PATH = '{{deploy_path}}/current/' . PATH;
 
 task('database:backup', function () {
-    run('mkdir -p ' . PATH);
+    run('mkdir -p ' . CURRENT_PATH);
 
     $env = run('cd {{deploy_path}}/current && cat .env');
     preg_match('/^DATABASE_URL=(.*?)$/m', $env, $url);
@@ -17,7 +18,14 @@ task('database:backup', function () {
     list($matches, $user, $password, $host, $port, $name) = $matches;
 
     $file = $name . '_' . date('ymdhis') . '.sql';
-    $path = PATH . "/{$file}";
+    $path = CURRENT_PATH . $file;
 
     run("mysqldump --single-transaction --host=\"{$host}\" --user=\"{$user}\" --password=\"{$password}\" {$name} > {$path}");
+});
+
+task('database:download', function () {
+    $files = run('ls ' . CURRENT_PATH);
+    $files = explode("\n", $files);
+    $file = end($files);
+    download(CURRENT_PATH . $file, PATH . $file);
 });
